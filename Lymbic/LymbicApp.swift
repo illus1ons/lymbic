c8567022d1cd07809ab8e07b45af532dee237567
Youngbae Yoo <youngbae.u@gmail.com>
2025-08-31 01:32:01 +0900

    Initial Commit

---
+++ Lymbic/LymbicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LymbicApp.swift
+//  Lymbic
+//
+//  Created by 유영배 on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct LymbicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
